d39a805ab1b2d376699ea8cb3643237b72deef73
Onsomujosh <Joshuaonsomu@gmail.com>
2023-08-31 10:34:30 -0700

    betty 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -5,7 +5,7 @@
  * @tree: Pointer to the root node of the tree to count the number of nodes.
  *
  * Return: If tree is NULL, the function must return 0.
- * 	Otherwise return node count.
+ *         Otherwise return node count.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
