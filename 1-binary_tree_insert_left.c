3969504b9738b737e77f4dac92d3bee322d51812
Onsomujosh <Joshuaonsomu@gmail.com>
2023-08-31 10:18:30 -0700

    mandatory tasks

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,7 +8,21 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_insert_left *New_node;
+	binary_tree_t *new;
 
-	New_node = malloc(sizeof(binary_tree_insert_left));
 	if (parent == NULL)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
+	parent->left = new;
+
+	return (new);
+}
